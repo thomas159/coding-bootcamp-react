62e0b4e08a89ac3e1dd6548be2e80207732aa6ea
thomas159 <thomas159@gmail.com>
2018-08-25 22:20:42 +0200

    all

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
+
 import registerServiceWorker from './registerServiceWorker'
 import './styles'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
 registerServiceWorker()
